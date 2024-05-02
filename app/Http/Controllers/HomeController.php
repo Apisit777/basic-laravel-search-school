5218f0e60808459769cde915109566c4c5101ca3
Apisit777 <apisit.jovanovic@gmail.com>
2024-05-02 22:30:29 +0700

    create mvc search school

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Home;
+use App\Models\Food;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,7 +13,63 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //
+        $foods = Food::select(
+            'id',
+            'name',
+            'price'
+        )
+        ->get();
+
+        // dd($foods);
+        return view('searchSchool', compact('foods'));
+    }
+
+    public function search_school(Request $request)
+    {
+        $limit = $request->input('length'); // limit per page
+        $request->merge([
+            'page' => ceil(($request->input('start') + 1) / $limit),
+        ]);
+
+        $name_number = $request->input('name_number');
+        $date_start = $request->input('date_start');
+        $date_end = $request->input('date_end');
+
+        $field_detail = ['booking_ins.name', 'booking_ins.tel'];
+
+        $data = Food::select(
+            'id',
+            'name',
+            'price')
+        ->orderBy('id', 'ASC');
+
+
+        if (null != $name_number) {
+            $data = $data->where(function ($data) use ($name_number, $field_detail) {
+                for ($i = 0; $i < count($field_detail); $i++) {
+                    $data->orWhere($field_detail[$i], 'like', '%'.$name_number.'%');
+                }
+            });
+        }
+
+        if (null != $date_start && null != $date_end) {
+            $data = $data->whereBetween('booking_ins.booking_date', ["$date_start", "$date_end"]);
+        } elseif (null != $date_start) {
+            $data = $data->where('booking_ins.booking_date', '=', "$date_start");
+        }
+
+        // dd($data);
+        $data = $data->paginate($limit);
+        $totalRecords = $data->total();
+        $totalRecordwithFilter = $data->count();
+        $response = [
+            'draw' => intval($request->draw),
+            'iTotalRecords' => $totalRecordwithFilter,
+            'iTotalDisplayRecords' => $totalRecords,
+            'aaData' => $data->items(),
+        ];
+
+        return response()->json($response);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('search_school', [HomeController::class, 'index']);
+Route::post('search_school', [HomeController::class, 'search_school'])->name('search_school');
 
 Route::get('/', function () {
     return view('welcome');
