3fca2b0524450f2699ccb242fd99b917b41a51e8
Apisit777 <apisit.jovanovic@gmail.com>
2024-05-06 17:21:25 +0700

    images upload

---
+++ database/migrations/2024_05_06_070843_create_product_images_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255)->nullable()->comment('ชื่อไฟล์');
+            $table->string('image_path', 255)->nullable()->comment('ชื่อพาร์ท');
+            $table->integer('seq')->default(0)->comment('ลำดับไฟล์');
+            $table->tinyInteger('status')->default(1)->comment('1= active 0= deactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_images');
+    }
+};
