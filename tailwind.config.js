b8ea5558a04b8e879f9af83410e84d24b109dacb
Apisit777 <apisit.jovanovic@gmail.com>
2024-05-24 17:35:50 +0700

    setting tailwind css dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // This is our star player for the dark mode!
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
