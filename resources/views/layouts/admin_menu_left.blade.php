ad20dfc8ee5b5a8a08bcbc439fde13373ef99573
Apisit777 <apisit.jovanovic@gmail.com>
2024-05-27 07:38:00 +0700

    dark mode & side menu

---
+++ resources/views/layouts/admin_menu_left.blade.php
@@ -0,0 +1,19 @@
+<style>
+    /*----------------sidebar-content----------------*/
+    .sidebar_menu {
+        position: fixed;
+        height: 100vh;
+        width: 288px;
+        background: rgba(97, 97, 97, 1);
+        /* box-shadow: 0 0 9px rgba(255, 255, 255, 0.5); */
+        z-index: 1;
+    }
+</style>
+
+    <div class="sidebar_menu">
+
+    </div>
+
+  <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
+  <script>
+  </script>
