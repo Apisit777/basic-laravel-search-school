4233f9480dfe190927fdd379e99265f589bea482
Apisit777 <apisit.jovanovic@gmail.com>
2024-05-27 23:58:42 +0700

    tailwinds css

--- resources/views/searchSchool.blade.php
+++ resources/views/searchSchool.blade.php
@@ -117,8 +117,8 @@
             </div>
         </div>
     </body>
-    
-    <script src="https://cdn.tailwindcss.com"></script> 
+
+    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/2.0.5/js/dataTables.js"></script>
