f1b789a01407238eda92d42efa93feef994bf0fa
Apisit777 <apisit.jovanovic@gmail.com>
2024-07-01 00:42:25 +0700

    fix menu

--- app/Models/menu_relation.php
+++ app/Models/menu_relation.php
@@ -9,5 +9,11 @@ class menu_relation extends Model
 {
     use HasFactory;
     public $timestamps = false;
-    protected $guarded = [];
+    // protected $guarded = [];
+
+    protected $fillable = ['menu_relations'];
+
+    protected $casts = [
+        'menu_relations' => 'array',
+    ];
 }
